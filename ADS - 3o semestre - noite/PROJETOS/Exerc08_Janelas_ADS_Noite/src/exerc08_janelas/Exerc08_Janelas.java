c22e9297af7db2aa62e3ecde72eb189554f44b9f
Alan Marcel <alanmarcel2@gmail.com>
2020-04-06 20:48:24 -0300

    projetos TODO 07, 08 e 09

---
+++ ADS - 3o semestre - noite/PROJETOS/Exerc08_Janelas_ADS_Noite/src/exerc08_janelas/Exerc08_Janelas.java
@@ -0,0 +1,27 @@
+
+package exerc08_janelas;
+
+import janelas.JanelaLogin;
+import javax.swing.JFrame;
+
+public class Exerc08_Janelas {
+
+    public static void main(String[] args) {
+        // DONE 01 - crie aqui um objeto da classe JanelaLogin de nome janelaLogin
+        JanelaLogin janelaLogin = new JanelaLogin();
+        // DONE 02 - através do objeto janelaLogin, acesse o método setSize 
+        ////////// e passe os seguintes valores: 300, 300
+        janelaLogin.setSize(300, 300);
+        // DONE 03 - através do objeto janelaLogin, acesse o método setDefaultCloseOperation 
+        ////////// e passe o seguinte valor: JFrame.EXIT_ON_CLOSE (realize importação)
+        janelaLogin.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        // DONE 04 - através do objeto janelaLogin, acesse o método setVisible 
+        ////////// e passe o seguinte valor: true
+        janelaLogin.setVisible(true);
+        
+        // DONE 05 - execute o projeto, digite valores nos campos e clique no botão!
+        ////////// o que acontece?
+        
+    }
+    
+}
---
+++ ADS - 3o semestre - noite/PROJETOS/Exerc08_Janelas_ADS_Noite/src/janelas/JanelaLogin.java
@@ -0,0 +1,100 @@
+
+package janelas;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class JanelaLogin extends JFrame {
+    private JPanel painel = new JPanel();
+    
+    private BoxLayout layoutPainel = new BoxLayout(painel, BoxLayout.PAGE_AXIS);
+    
+    private JLabel txtLogin = new JLabel("Informe seus dados");
+    private JLabel txtEmail = new JLabel("E-mail:");
+    private JLabel txtSenha = new JLabel("Senha:");
+    
+    private JTextField campoEmail = new JTextField(20);
+    private JPasswordField campoSenha = new JPasswordField(20);
+    
+    private JButton botaoOK = new JButton("Entrar");
+
+    public JanelaLogin() {
+        
+        painel.setLayout(layoutPainel);
+        campoEmail.setMaximumSize( campoEmail.getPreferredSize() );
+        campoSenha.setMaximumSize( campoSenha.getPreferredSize() );
+        
+        txtLogin.setAlignmentX(CENTER_ALIGNMENT);
+        txtEmail.setAlignmentX(CENTER_ALIGNMENT);
+        txtSenha.setAlignmentX(CENTER_ALIGNMENT);
+        campoEmail.setAlignmentX(CENTER_ALIGNMENT);
+        campoSenha.setAlignmentX(CENTER_ALIGNMENT);
+        botaoOK.setAlignmentX(CENTER_ALIGNMENT);
+        
+        // DONE 06 - vamos programar aqui a ação de clique do botão
+        
+        // o método addActionListener é chamado através do objeto botaoOK
+        botaoOK.addActionListener(new ActionListener() {
+            
+            // dentro deste método, chama-se o método actionPerformed 
+            // repare que o actionPerformed está sendo sobrescrito!!! (override):
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                // DONE 07 - aqui dentro, crie uma variável do tipo String de nome
+                ////////// emailDigitado e atribua a ela o valor de retorno 
+                ////////// do método getText que deve ser chamado através do 
+                ////////// objeto campoEmail
+                String emailDigitado = campoEmail.getText();
+                // DONE 08 - crie agora outra variável do tipo String de nome
+                ////////// senhaDigitada e atribua a ela o valor de retorno 
+                ////////// do método getPassword que deve ser chamado através do 
+                ////////// objeto campoSenha
+                String senhaDigitada = String.valueOf( campoSenha.getPassword() );
+                
+                // DONE 09 - por que está dando erro?
+                ////////// R: O erro ocorre, porque o método getPassword está 
+                // retornando um valor do tipo char[] e estamos tentando
+                // armazenar esse valor em uma variável do tipo String, gerando
+                // incompatibilidade de tipos.
+                
+                // DONE 10 - realize a conversão para String através do método String.valueOf
+                
+                // DONE 11 - programe a seguinte verificação:
+                ////////// se a senha digitada for igual a "senha!@#" e o email "admin@sistema.com"
+                ////////// então mostre uma caixa de JOptionPane da seguinte forma:
+                if ( senhaDigitada.equals("senha!@#") && 
+                        emailDigitado.equals("admin@sistema.com") ) {
+                    JOptionPane.showMessageDialog(null, "Senha Correta", "Sistema.com", 
+                            JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                    // senão, mostre a seguinte caixa:
+                    JOptionPane.showMessageDialog(null, "Senha Incorreta", "Sistema.com", 
+                            JOptionPane.ERROR_MESSAGE);
+                }
+                ////////// faça importações necessárias
+                
+                // DONE 12 - execute o projeto, faça os testes. o que acontece? 
+                ////////// LEGAL NÉ??? ;)
+                
+            }
+        });
+        
+        painel.add(txtLogin);
+        painel.add(txtEmail);
+        painel.add(campoEmail);
+        painel.add(txtSenha);
+        painel.add(campoSenha);
+        painel.add(botaoOK);
+        
+        this.add(painel);
+    }
+    
+}
